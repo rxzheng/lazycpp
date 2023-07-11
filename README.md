9710cfb32a68e9cad65542a5891534b1b25d6da3
rxzheng <richardxzheng@outlook.com>
2023-07-11 13:15:26 +1000

    Created main.cpp and README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lazycpp
+Program to add at the front of C++ projects to be lazy
+
+Credits:
+Matthew
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define int long long
+#define uint unsigned long long
+#define float long double
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define ub upper_bound
+#define lb lower_bound
+#define endl "\n"
+#define all(x) begin(x), end(x)
+#define ret return
+#define ari2 array<int, 2>
+#define ari3 array<int, 3>
+#define MOD 0
+#define Mod(n) (n%MOD + MOD) % MOD
+using namespace std;
+int sublimeisactingweird = 0;
